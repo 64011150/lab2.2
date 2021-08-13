a3e1e757d24a63cb757335affbaf7847fc68661d
Acer <Acer@DESKTOP-ANUJO1P>
2021-08-13 16:55:01 +0700

    Add project files.

---
+++ lab2.2/Source.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main() {
+	int a, b;
+	printf("Enter first number : ");
+	scanf_s("%d", &a);
+	printf("Enter second number : ");
+	scanf_s("%d", &b);
+	int d = 1;
+	int x = 0;
+	while (x == 0) {
+		x = 1;
+		for (int i = 2; i < a || i < b; i++) {
+			if (a % i == 0 && b % i == 0) {
+				a = a / i;
+				b = b / i;
+				d = d * i;
+				x = 0;
+				break;
+			}
+		}
+		if (x == 1) {
+			break;
+		}
+	}
+	printf("Greatest common divisor = %d", d);
+	return 0;
+}
